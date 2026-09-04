48bd77102131ad4dc4b0ef63e6301be3236faae9
Davi Nicacio <daviniccacio@gmail.com>
2026-09-04 17:13:39 -0300

    fix: erro no simulador + teste de fonte

--- metro.config.js
+++ metro.config.js
@@ -1,11 +1,7 @@
 const { getDefaultConfig } = require("expo/metro-config");
 const { withNativeWind } = require("nativewind/metro");
 
-// 1. Obtém a configuração padrão do Expo
+/** @type {import('expo/metro-config').MetroConfig} */
 const config = getDefaultConfig(__dirname);
 
-// 2. Adiciona suporte a extensões .cjs e .mjs (necessário para o Moti e Framer Motion)
-config.resolver.sourceExts.push("cjs", "mjs");
-
-// 3. Exporta a configuração integrada ao NativeWind (Tailwind)
 module.exports = withNativeWind(config, { input: "./global.css" });
